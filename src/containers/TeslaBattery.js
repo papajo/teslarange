13cdb61d41318ceac75712e75542db9afa12620e
papajo <joshipv2@gmail.com>
2018-11-19 20:33:25 -0500

    TeslaCar component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
 import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
 
 class TeslaBattery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            carstats: [],
+            config: {
+                speed: 55,
+                temperature: 20,
+                climate: true,
+                wheels: 19
+            }
+        }
+    }
     render() {
+        //ES6 Object destructuring Syntax,
+        //takes out required values and create references to them
+        const { config } = this.state;  // <-- Object destructuring syntax
         return (
             <form className="tesla-battery">
                 <h1>Range Per Charge</h1>
+                <TeslaCar wheelsize={config.wheels}/>
                 <TeslaNotice />
             </form>
         )
